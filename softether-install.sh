870fd00f7ffd6bbd61e72ef4aa3b31749088b9bd
Euler <46149630+EulerABC@users.noreply.github.com>
2026-09-02 22:15:15 +0800

    Update softether-install.sh

--- softether-install.sh
+++ softether-install.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
-
 set -e
-
 apt update
 apt install -y build-essential wget
-
 mkdir /etc/softether
 cd /etc/softether
-
 wget https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.44-9807-rtm/softether-vpnserver-v4.44-9807-rtm-2025.04.16-linux-x64-64bit.tar.gz
 tar -xzf softether-vpnserver-v4.44-9807-rtm-2025.04.16-linux-x64-64bit.tar.gz
-
 cd vpnserver
 make
-
 cat > /etc/systemd/system/vpnserver.service <<'EOF'
 [Unit]
 Description=SoftEther VPN Server
 After=network.target
-
 [Service]
 Type=forking
 ExecStart=/etc/softether/vpnserver/vpnserver start
 ExecStop=/etc/softether/vpnserver/vpnserver stop
 ExecReload=/etc/softether/vpnserver/vpnserver restart
 Restart=on-failure
-
 [Install]
 WantedBy=multi-user.target
 EOF
-
 systemctl daemon-reload
 systemctl enable --now vpnserver
