2b45cff602fcd705b33861ad36e05de4be37d701
Euler <46149630+EulerABC@users.noreply.github.com>
2026-09-02 22:01:31 +0800

    Update softether-install.sh

--- softether-install.sh
+++ softether-install.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-INSTALL_DIR="/etc/softether"
-FILE="softether-vpnserver-v4.44-9807-rtm-2025.04.16-linux-x64-64bit.tar.gz"
-URL="https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.44-9807-rtm/$FILE"
-
 apt update
 apt install -y build-essential wget
 
@@ -13,7 +9,7 @@ mkdir -p /etc/softether
 cd /etc/softether
 
 wget -O "softether-vpnserver-v4.44-9807-rtm-2025.04.16-linux-x64-64bit.tar.gz" "https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.44-9807-rtm/softether-vpnserver-v4.44-9807-rtm-2025.04.16-linux-x64-64bit.tar.gz"
-tar -xzf "$FILE"
+tar -xzf softether-vpnserver-v4.44-9807-rtm-2025.04.16-linux-x64-64bit.tar.gz
 
 cd vpnserver
 make
