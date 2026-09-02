bf349c6e67bb140df764748df031c5874d4b5e54
Euler <46149630+EulerABC@users.noreply.github.com>
2026-09-02 14:55:34 +0800

    Add files via upload

---
+++ softether-install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+INSTALL_DIR="/etc/softether"
+FILE="softether-vpnserver-v4.44-9807-rtm-2025.04.16-linux-x64-64bit.tar.gz"
+URL="https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.44-9807-rtm/$FILE"
+
+apt update
+apt install -y build-essential wget
+
+mkdir -p "$INSTALL_DIR"
+cd "$INSTALL_DIR"
+
+wget -O "$FILE" "$URL"
+tar -xzf "$FILE"
+
+cd vpnserver
+make
+
+cat > /etc/systemd/system/vpnserver.service <<'EOF'
+[Unit]
+Description=SoftEther VPN Server
+After=network.target
+
+[Service]
+Type=forking
+ExecStart=/etc/softether/vpnserver/vpnserver start
+ExecStop=/etc/softether/vpnserver/vpnserver stop
+ExecReload=/etc/softether/vpnserver/vpnserver restart
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable --now vpnserver
